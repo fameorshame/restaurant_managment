2da7bd13b525e02369f8474ba13496257850ab3e
DOUBLE U <realnieopasnie@gmail.com>
2017-12-24 20:21:48 +0600

    Update restaurant.py

--- restaurant.py
+++ restaurant.py
@@ -1,1 +1,7 @@
-
+class Restaurant():
+  def __init__(self, served_guests, complaints, revenue):
+    self.served_guests = served_guests
+    self.complaints = complaints
+    self.revenue = revenue
+  def 
+    
